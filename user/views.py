c8730a864f4c8f92bb80d8351085ad490b7126f3
EC2 Default User <ec2-user@ip-172-31-11-152.ap-northeast-1.compute.internal>
2020-11-13 06:28:50 +0000

    fix indent

--- user/views.py
+++ user/views.py
@@ -49,7 +49,7 @@ class DashboardView(LoginRequiredMixin, TemplateView):
 
 class OnlyYouMixin(UserPassesTestMixin):
     raise_exception = True
-
+    
     def test_func(self):
         user = self.request.user
         return user.pk == self.kwargs['pk'] or user.is_superuser
